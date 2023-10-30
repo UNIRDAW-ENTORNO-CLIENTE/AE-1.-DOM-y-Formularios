85d3217eedf984c7666810f7a6b64d21a401bd0a
luixmydev <131276444+luixmydev@users.noreply.github.com>
2023-10-30 22:05:04 +0100

    Requerimiento 2

--- Requerimiento2/script.js
+++ Requerimiento2/script.js
@@ -1,71 +1,76 @@
-// Función que se ejecutará cuando la ventana esté completamente cargada
-window.onload = function () {
-    // Obtén los elementos del formulario
-    let nombreInput = document.getElementById("nombre");
-    let direccionInput = document.getElementById("direccion");
-    let movilInput = document.getElementById("movil");
-    let emailInput = document.getElementById("email");
-    let pizzaRadios = document.querySelectorAll('input[type="radio"][name="Pizza"]');
-    let ingredientCheckboxes = document.querySelectorAll('input[type="checkbox"][name="Condiciones"]    ');
-    let botonProcesarPedido = document.querySelector('.boton');
 
-    // Agrega un evento al botón para procesar el pedido
-    botonProcesarPedido.addEventListener("click", function () {
-        // Verifica que los campos obligatorios estén completos
-        if (nombreInput.value.trim() === "" || direccionInput.value.trim() === "" || movilInput.value.trim() === "" || emailInput.value.trim() === "") {
-            alert("Por favor, complete los campos obligatorios: Nombre, Dirección, Número de móvil y Email.");
-            return;
+function checkparam(){
+    //COMPROBACION DE LOS INPUT TEXT
+    if (nombre.value.trim() == "") {
+        alert('Falta rellenar el campo "Nombre"');
+        return false;
+    } else if(direccion.value.trim() == ""){
+        alert('Falta rellenar el campo "Direccion"');
+        return false;
+    } else if(telefono.value.trim() == ""){
+        alert('Falta rellenar el campo "Telefono"');
+        return false;
+    } else if(email.value.trim() == ""){
+        alert('Falta rellenar el campo "Email"');
+        return false;
+    }
+    //COMPROBACION DEL TAMAÑO
+    let Tamaño = document.getElementsByName("Tamaño");
+    let checked = false
+    let tam = ""
+    Tamaño.forEach(Value => {
+        if(Value.checked){
+            //GUARDAMOIS EL TAMAÑO PARA CALCULAR EL PRECIO DESPUES
+            tam=Value.value
+            //HAY UN TAMAÑO
+            checked = true
         }
-
-        // Verifica que se haya seleccionado un tamaño de pizza
-        let pizzaSeleccionada = false;
-        for (let i = 0; i < pizzaRadios.length; i++) {
-            if (pizzaRadios[i].checked) {
-                pizzaSeleccionada = true;
-                break;
-            }
-        }
-        if (!pizzaSeleccionada) {
-            alert("Por favor, seleccione un tamaño de pizza.");
-            return;
-        }
-
-        // Verifica que se haya seleccionado al menos un ingrediente
-        let ingredientesSeleccionados = false;
-        for (let i = 0; i < ingredientCheckboxes.length; i++) {
-            if (ingredientCheckboxes[i].checked) {
-                ingredientesSeleccionados = true;
-                break;
-            }
-        }
-        if (!ingredientesSeleccionados) {
-            alert("Por favor, seleccione al menos un ingrediente.");
-            return;
-        }
-
-        // Cálculo del precio de la pizza según el tamaño
-        let precioPizza = 0;
-        if (pizzaRadios[0].checked) {
-            precioPizza = 5;
-        } else if (pizzaRadios[1].checked) {
-            precioPizza = 10;
-        } else if (pizzaRadios[2].checked) {
-            precioPizza = 15;
-        }
-
-        // Cálculo de añadir 1 euro por cada ingrediente seleccionado
-        let precioIngredientes = 0;
-        for (let i = 0; i < ingredientCheckboxes.length; i++) {
-            if (ingredientCheckboxes[i].checked) {
-            precioIngredientes += 1; 
-            }
-        }
-
-
-        //Calcula el precio total del pedido
-        let precioTotal = precioPizza + precioIngredientes; 
-        
-        // Muestra el precio total al usuario
-        alert("El precio total del pedido es: " + precioTotal + "€");
     });
-};
+    //COMPROBAMOS QUE HABIA UN TAMAÑO
+    if(!checked){
+        alert('Falta elegir un "Tamaño"')
+        return false
+    }
+    //COMPROBACION DE LOS INGREDIENTES
+    let ingredientes = document.getElementsByName("ingrediente");
+    let cont = 0
+    //RECORREMOS EL ARRAY DE LOS INGREDIENTES
+    ingredientes.forEach(Value => {
+        if(Value.checked){
+            //SUMAMOS LOS INGREDIENTES CHEKADOS
+            cont++
+        }
+    });
+    //SI NO HAY INGREDIENTES, SALTA EL AVISO
+    if(cont==0){
+        alert('Falta elegir mínimo un "Ingrediente"')
+        return false
+    }
+    //CALCULA EL PRECIO FINAL
+    let price = sumPrice(tam,cont)
+
+    alert("Precio final de la pizza: "+price+"$")
+}
+//FUNCION CALCULAR EL PRECIO FINAL 
+function sumPrice(pizzaSize,numIngre){
+    let finalPrice = 0
+    //DEPENDE DEL TAMAÑO, SUMA EL PRECIO
+    switch(pizzaSize){
+        case 'Pequeña':
+            finalPrice += 5
+            break;
+        case 'Mediana':
+            finalPrice += 10
+            break;    
+        case 'Grande':
+            finalPrice += 15
+            break;
+    }
+    //RETORNA EL PRECIO FINAL DE LA PIZZA
+    return finalPrice+numIngre
+}
+
+window.onload = function(){
+    //CUANDO SE ENVIA EL FORMULARIO CHEKEA LOS PARAMETROS SOLO SI LA WEB HTML YA HA CARGADO
+    formulario.onsubmit = checkparam
+}
